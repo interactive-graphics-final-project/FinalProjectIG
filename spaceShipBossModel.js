33d575c81db0bb0559c8ae93fecf3263a0f19914
Alegi <ale.giannetti@hotmail.it>
2019-06-08 22:39:47 +0200

    fix texture boss

--- spaceShipBossModel.js
+++ spaceShipBossModel.js
@@ -25,22 +25,13 @@ var SpaceShipBossModel = function (inName) {
 
     return this;
 };
+var textureLoader = new THREE.TextureLoader();
+textureLoader.load('./models/texture/t3.jpg', function (texture) {
 
-SpaceShipBossModel.texture = new THREE.ImageUtils.loadTexture('./models/texture/t3.jpg');
 SpaceShipBossModel.materials = new THREE.MeshPhongMaterial({
-    map: SpaceShipBossModel.texture,
-    bumpMap: SpaceShipBossModel.texture,
-    bumpScale: 0.3,
-    color: 0xffffff
-});
-SpaceShipBossModel.material3 = new THREE.MeshPhongMaterial({map: SpaceShipBossModel.texture, bumpMap: SpaceShipBossModel.texture, bumpScale: 0.3});
-SpaceShipBossModel.materials2 = new THREE.MeshPhongMaterial({
-    map: SpaceShipBossModel.texture,
-    bumpMap: SpaceShipBossModel.texture,
-    bumpScale: 0.3,
-    color: 0xffffff
-});
-SpaceShipBossModel.material4 = new THREE.MeshBasicMaterial({color: 0xffffff, specular: 0xffffff, ambient: 0xffffff});
+    map: texture
+})});
+
 
 SpaceShipBossModel.prototype.createHead = function () {
     head = new THREE.Object3D();
@@ -64,25 +55,25 @@ SpaceShipBossModel.prototype.createBody = function () {
 
     var mainBody = new THREE.CylinderBufferGeometry(1, 1, 6, 36);
     var mainBodyBox = new THREE.Mesh(mainBody,
-        SpaceShipBossModel.material3);
+        SpaceShipBossModel.materials);
     mainBodyBox.position.y = 80;
     body.add(mainBodyBox);
 
     var engineProtection = new THREE.TorusBufferGeometry(1, 0.4, 30, 30);
     var engineProtectionBox = new THREE.Mesh(engineProtection,
-        SpaceShipBossModel.material3);
+        SpaceShipBossModel.materials);
     engineProtectionBox.position.y = 77.1;
     engineProtectionBox.rotation.x = Math.PI / 2;
     body.add(engineProtectionBox);
 
     var engineGeometry = new THREE.CylinderBufferGeometry(0.29, 0.5, 1, 36);
-    var engineBox = new THREE.Mesh(engineGeometry, SpaceShipBossModel.materials2);
+    var engineBox = new THREE.Mesh(engineGeometry, SpaceShipBossModel.materials);
     engineBox.position.y = 77.1;
     body.add(engineBox);
 
     // tetrahedron under body
     var cargoGeometry = new THREE.CylinderGeometry(0.84, 0.2, 4.3, 15);
-    var cargoBox = new THREE.Mesh(cargoGeometry, SpaceShipBossModel.materials2);
+    var cargoBox = new THREE.Mesh(cargoGeometry, SpaceShipBossModel.materials);
     cargoBox.position.y = 80.5;
     cargoBox.position.x = 2.7;
 
@@ -90,7 +81,7 @@ SpaceShipBossModel.prototype.createBody = function () {
     body.add(cargoBox);
 
     var weightCargoGeometry = new THREE.CylinderGeometry(0.15, 0.3, 1.8, 15);
-    var weightCargoBox = new THREE.Mesh(weightCargoGeometry, SpaceShipBossModel.materials2);
+    var weightCargoBox = new THREE.Mesh(weightCargoGeometry, SpaceShipBossModel.materials);
     weightCargoBox.position.y = 80.5;
     weightCargoBox.position.x = 5;
 
@@ -105,13 +96,13 @@ SpaceShipBossModel.prototype.createLeftWing = function () {
 
     var wingGeometry = new THREE.BoxBufferGeometry(0.2, 1.2, 3.2);
     var wingBox = new THREE.Mesh(wingGeometry,
-        SpaceShipBossModel.material3);
+        SpaceShipBossModel.materials);
     wingBox.position.z = 2.5;
     leftWing.add(wingBox);
 
     var wingWeightGeometry = new THREE.CylinderBufferGeometry(0.2, 0.2, 1.5);
     var wingWeightBox = new THREE.Mesh(wingWeightGeometry,
-        SpaceShipBossModel.material3);
+        SpaceShipBossModel.materials);
     wingWeightBox.position.z = 4;
     leftWing.add(wingWeightBox);
 
@@ -125,13 +116,13 @@ SpaceShipBossModel.prototype.createRightWing = function () {
     var wingGeometry = new THREE.BoxBufferGeometry(0.2, 1.2, 3.2);
 
     var bodyBox = new THREE.Mesh(wingGeometry,
-        SpaceShipBossModel.material3);
+        SpaceShipBossModel.materials);
     bodyBox.position.z = -2.5;
     rightWing.add(bodyBox);
 
     var wingWeightGeometry = new THREE.CylinderBufferGeometry(0.2, 0.2, 1.5);
     var wingWeightBox = new THREE.Mesh(wingWeightGeometry,
-        SpaceShipBossModel.material3);
+        SpaceShipBossModel.materials);
     wingWeightBox.position.z = -4;
     rightWing.add(wingWeightBox);
 
@@ -145,13 +136,13 @@ SpaceShipBossModel.prototype.createBaseCannon = function () {
     cannon = new THREE.Object3D();
 
     var baseCannonGeometry = new THREE.SphereBufferGeometry(0.8, 32);
-    var bodyBox1 = new THREE.Mesh(baseCannonGeometry, SpaceShipBossModel.material3);
+    var bodyBox1 = new THREE.Mesh(baseCannonGeometry, SpaceShipBossModel.materials);
     bodyBox1.position.y = 1.62;
     bodyBox1.position.x = -0.9;
     baseCannon.add(bodyBox1);
 
     var CannonGeometry = new THREE.CylinderBufferGeometry(0.05, 0.18, 1.5);
-    var bodyBox2 = new THREE.Mesh(CannonGeometry, SpaceShipBossModel.material3);
+    var bodyBox2 = new THREE.Mesh(CannonGeometry, SpaceShipBossModel.materials);
     bodyBox2.position.y = 2.5;
     bodyBox2.position.x = -1.4;
 
